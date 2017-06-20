336bc68bbe4293c992fb2479bd107412ada2d532
prabu <PRABU.D@TRINGAPPS.com>
2017-06-20 11:16:36 +0530

    Commiting the latest changes for android related code

---
+++ URLOOP/src/test/java/androidTestCase/Android_Login_page.java
@@ -0,0 +1,81 @@
+package androidTestCase;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+import androidTestScripts.Android_LoginTestScripts;
+import excelReadWrite.ExcelRead;
+import io.appium.java_client.android.AndroidDriver;
+import readingPropertiesFile.ReadConfigProperties;
+import webTestScripts.LoginTestScripts;
+
+import org.testng.annotations.BeforeTest;
+
+
+public class Android_Login_page {
+	
+public static AndroidDriver<WebElement> dr;
+public static Connectionintialization conn = new Connectionintialization();
+
+
+
+
+Properties propertiesConfig;
+static String excelURL;
+static String androidexcelFileName;
+static Android_LoginTestScripts android_LoginTestScripts;
+
+
+public Android_Login_page() throws IOException{
+	
+ android_LoginTestScripts = new Android_LoginTestScripts();
+	
+	
+	propertiesConfig = ReadConfigProperties.ReadConfigPropertiesFile();
+	excelURL =	propertiesConfig.getProperty("ExcelURL");
+	androidexcelFileName =	propertiesConfig.getProperty("AndroidExcelFileName");
+	
+}
+
+
+
+@BeforeTest
+public void basicConfiguration() throws IOException, InterruptedException{
+	
+	android_LoginTestScripts.basicConfiguration();
+	
+}
+
+
+@Test(priority=1)
+public static void Invalidlogin() throws InterruptedException, IOException
+{
+	Thread.sleep(2000);
+	android_LoginTestScripts.Invalidlogin(ExcelRead.readExcelByKeyWord(excelURL,androidexcelFileName,"Login","No userpwd","EmailID"), 
+			ExcelRead.readExcelByKeyWord(excelURL,androidexcelFileName,"Login","No userpwd","Password"));
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
--- URLOOP/src/test/java/androidTestScripts/Android_LoginTestScripts.java
+++ URLOOP/src/test/java/androidTestScripts/Android_LoginTestScripts.java
@@ -1,5 +1,76 @@
 package androidTestScripts;
 
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import androidTestCase.Connectionintialization;
+import io.appium.java_client.android.AndroidDriver;
+import objectRepositoryReader.ObjectRepositoryStaticReader;
+import readingPropertiesFile.ReadConfigProperties;
+
 public class Android_LoginTestScripts {
+	
+	Properties propertiesConfig;
+	
+	static String AndroidloginXmlURL;
+	static String excelURL;
+	static String excelFileName;
+	
+	
+	/*public static AndroidDriver<WebElement> dr;*/
+	public static Connectionintialization conn = new Connectionintialization();
+	
+	public void basicConfiguration() throws IOException, InterruptedException{
+		propertiesConfig = ReadConfigProperties.ReadConfigPropertiesFile();
+				
+		AndroidloginXmlURL =	propertiesConfig.getProperty("AndroidLoginXmlURL");
+		excelURL =	propertiesConfig.getProperty("ExcelURL");
+		excelFileName =	propertiesConfig.getProperty("ExcelFileName");
+			
+		conn.connection();
+		
+	}
+	
+public void Invalidlogin(String userName,String password) throws InterruptedException{
+	
+	conn.dr.findElement(By.id(ObjectRepositoryStaticReader.oBjectXmlReader(AndroidloginXmlURL,"Username","Id")))
+	.sendKeys(userName);
+	conn.dr.findElement(By.id(ObjectRepositoryStaticReader.oBjectXmlReader(AndroidloginXmlURL,"Password","Id")))
+	.sendKeys(password);
+	conn.dr.hideKeyboard();
+	conn.dr.findElement(By.name(ObjectRepositoryStaticReader.oBjectXmlReader(AndroidloginXmlURL,"SignIn","Text")))
+	.click();
+	Thread.sleep(2000);
+	if(conn.dr.findElement(By.id(ObjectRepositoryStaticReader.oBjectXmlReader(AndroidloginXmlURL,"Errormessage","Id"))).isDisplayed()==true)
+	{
+	
+		System.out.println("Alert message displayed");
+		
+		String Text = conn.dr.findElement(By.id(ObjectRepositoryStaticReader.oBjectXmlReader(AndroidloginXmlURL,"Errormessage","Id"))).getText();
+		
+		System.out.println("Text IS :"+Text);
+		
+		if(Text.trim().equals("Please enter a valid credentials and try again")
+				){
+			System.out.println("Text are same");
+			}
+		else
+		{
+			System.out.println("Text are not same");
+		}
+	}
+	
+	else
+	{
+		
+		System.out.println("Alert message not displayed");
+		
+	}
+		
+	}
+	
 
 }
